8f52f0858b3c6dbfbc011996749e0103724e1716
l.sosnowski <albercik33@icloud.com>
2022-06-11 09:05:50 +0200

    add1

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ lastname = "Smith"
 console.log(lastname);
 
 // typy proste: string (ciąg znaków), number (liczba)
+
+// const result = "To jest " + firstname + " " + lastname + " " + "i mieszka w Londynie"
+
+// const result2 = `To jest ${firstname} ${lastname} i mieszka w londynie`; 
+// console.log(result);
+
+const city = "Katowice";
+const day = "Sobota";
+let temp = "20";
+
+
+const result3 = `Nasze miasto to ${city}. Dziś jest ${day}, na zewnątrz jest ${temp} stopni.`;
+
+console.log(result3);
