281f2f4e3c0ed4803d2a8639ce69b045e23147f5
l.sosnowski <albercik33@icloud.com>
2022-06-11 08:51:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// var, const, let
+
+// var firstname = "Jan";
+// var lastname = "Kowalski";
+
+
+// deklaracja zmiennej - tutakj powstaje
+const firstname = "Jan";
+// firstname = "John";
+
+console.log(firstname);
+
+let lastname = "Kowalski";
+lastname = "Smith"
+
+console.log(lastname);
+
+// typy proste: string (ciąg znaków), number (liczba)
